8bb6a3625a5a0eae00a4611c3687ddacc38544c6
Yuriy <nikorezar@gmail.com>
2022-07-10 13:55:13 +0300

    feat: structures of project the ended

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TracksModule } from './tracks/tracks.module';
+import { AlbumsModule } from './albums/albums.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TracksModule, AlbumsModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  const PORT = process.env.PORT || 5000;
+  app.setGlobalPrefix('/api');
 
-  await app.listen(3000);
+  await app.listen(PORT, () => console.log(`Server start with ${PORT} port`));
 }
 
 bootstrap();
